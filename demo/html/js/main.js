6ee8964b855e73d8256bf9fc29efad54a3bf077f
nevergiveup-j <nevergivejun@163.com>
2016-07-18 16:13:09 +0800

    demo

---
+++ demo/html/js/main.js
@@ -0,0 +1,22 @@
+(function() {
+
+	var Main = {
+		init: function() {
+			var self = this;
+
+			this.bind();
+		},
+		bind: function() {
+			var self = this;
+
+
+
+		}
+	}
+
+
+  $(function(){
+    Main.init();
+  })
+
+})();
